ae6079badf46ede0be0649d80333438a5a9f716f
Dastageer <pinjaridastageer@gmail.com>
2025-01-17 21:43:55 +0530

    login

--- ngmodule-app/src/app/app.module.ts
+++ ngmodule-app/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './main-app/header/header.component';
-import { FooterComponent } from './main-app/footer/footer.component';
+// import { HeaderComponent } from './main-app/header/header.component';
+// import { FooterComponent } from './main-app/footer/footer.component';
 import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
+    // HeaderComponent,
+    // FooterComponent,
   ],
   imports: [
     BrowserModule,
